03d542e1fa7ca81d386ea715a8a062eec02eb271
jamesnitz <jamesanitz2@gmail.com>
2020-05-29 11:19:38 -0500

    dollars to a piggy bank, piggy bank to dollars, DONE

--- coins_to_cash.py
+++ coins_to_cash.py
@@ -16,3 +16,35 @@ def calc_dollars(**coins):
   print(total)
 
 calc_dollars(pennies= 342, nickels=9, dimes=32, quarters=4)
+
+dollarAmount = 8.69
+piggyBank = {
+    "pennies": 0,
+    "nickels": 0,
+    "dimes": 0,
+    "quarters": 0
+}
+
+def coin_star(money):
+  while True:
+    if money > .25:
+      quarters = (money // .25)
+      money = money - (quarters * .25)
+      money = round(money, 2)
+      piggyBank["quarters"] = quarters
+    elif money > .10:
+      dimes = (money // .10)
+      money = money - (dimes * .10)
+      piggyBank["dimes"] = dimes
+    elif money > .05:
+      nickels = (money // .05)
+      money = money - (nickels * .05)
+      money = round(money, 2)
+      piggyBank["nickels"] = nickels
+    elif money > .01:
+      pennies = (money // .01)
+      money = money - (pennies * .01)
+      piggyBank["pennies"] = pennies
+      break
+
+coin_star(dollarAmount)
